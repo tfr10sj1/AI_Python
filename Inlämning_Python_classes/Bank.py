c05d46a117b5839742179f358bf47ec50559612b
tfr10sj1 <32736648+tfr10sj1@users.noreply.github.com>
2022-11-17 00:27:12 +0100

    _load method in bank class
    Working method to get a nice dictionary over all costomers with there accounts from a ftext file

--- Inlämning_Python_classes/Bank.py
+++ Inlämning_Python_classes/Bank.py
@@ -22,31 +22,24 @@ class Bank:
             #account_info.append(customer_id)
             #account_info.append(name)
             #account_info.append(pnr)
-            info = str(self.name) + ":" + str(self.pnr)
+            info = name + ":" + pnr
             for item in accounts:
                 if("#" in item):
                     balance, new_account_nbr = item.split("#")
                     #account_info.append(balance)
                     #account_info.append("#")
                     #account_info.append(new_account_nbr)
-                    info += ":" + str(balance) + "#" + str(new_account_nbr)
+                    info += ":" + balance + "#" + new_account_nbr
                 else:
                     #print(item)
                     #account_info.append(item)
                     info += ":" + str(item)
-                    self.customer_info[str(customer_id)] = info 
-                    info = ""      
-        print(self.customer_info)
+            self.customer_info[customer_id] = info 
+                #info = ""      
+        #print(self.customer_info)
 
-    def ger_customers():
-        with open("customers.txt", 'r') as file:
-            data = [line.strip() for line in file]
-  
-        #print(data)
-        # iterating through data
-        for line in data:
-            Customer_id, firstname, pnr, *account_type = line.split(":")
-            print("name/Prn:",firstname, pnr)
+    def ger_customers(self):
+        return self.customer_info
     
     def add_customer(name, pnr):
         "add customer"
